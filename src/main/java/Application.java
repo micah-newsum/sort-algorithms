995780ab2561a3be39b0d256474ad43389c73aa5
Micah Newsum <mnewsum@glenair.com>
2021-03-02 08:20:43 -0800

    Add method to print array.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,21 @@
+import java.util.Arrays;
+
 public class Application {
   public static void main(String[] args) {
     System.out.println("-----Bubble Sort-----");
     int[] ints = createUnsortedArray();
-    
+    System.out.println("Array Before Sort");
+    printArray(ints);
+    System.out.println("Array After Sort");
+    printArray(ints);
   }
 
   public static int[] createUnsortedArray(){
     return new int[] {20, 35, -15, 7, 55, 1, -22};
+  }
+
+  public static void printArray(int[] ints){
+    Arrays.stream(ints)
+        .forEach(System.out::println);
   }
 }
