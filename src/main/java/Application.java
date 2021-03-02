9ff87d7df18010b39141bb37d809dfea2a302e8e
Micah Newsum <mnewsum@glenair.com>
2021-03-02 08:43:54 -0800

    Implement selection sort and fix bug where algorithms are not executed.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,11 +2,24 @@ import java.util.Arrays;
 
 public class Application {
   public static void main(String[] args) {
+    int[] ints;
+
+    // bubble sort
     System.out.println("-----Bubble Sort-----");
-    int[] ints = createUnsortedArray();
+    ints = createUnsortedArray();
     System.out.println("Array Before Sort");
     printArray(ints);
     System.out.println("Array After Sort");
+    bubbleSort(ints);
+    printArray(ints);
+
+    // selection sort
+    System.out.println("-----Selection Sort-----");
+    ints = createUnsortedArray();
+    System.out.println("Array Before Sort");
+    printArray(ints);
+    System.out.println("Array After Sort");
+    selectionSort(ints);
     printArray(ints);
   }
 
@@ -21,7 +34,7 @@ public class Application {
 
   /**
    * Bubble sort is named as such because the sorted values seemingly bubble to the end of the array.
-   * The time complexity for the algorithm is O(n^2) because for each array input n, the algorithm requires a full traversal of the inner loop.
+   * The time complexity for this algorithm is O(n^2) because for each array input n, the algorithm may traverse the entire inner loop.
    * Therefore, n*n = n^2.
    * @param ints
    */
@@ -48,5 +61,25 @@ public class Application {
     int temp = ints[i];
     ints[i] = ints[j];
     ints[j] = temp;
+  }
+
+  /**
+   * Selection sort is named as such because it selects the largest value and inserts it at the beginning of the unsorted array partition.
+   * The time complexity for this algorithm is O(n^2) because for each array input n, the algorithm may require a comparison with every other array element n.
+   * Therefore, n*n = n^2.
+   * @param ints
+   */
+  public static void selectionSort(int[] ints){
+    for (int lastUnsortedIndex = ints.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
+      int indexOfLargest = 0;
+
+      for (int i = 1; i <= lastUnsortedIndex; i++){
+        if (ints[i] > ints[indexOfLargest]){
+          indexOfLargest = i;
+        }
+      }
+
+      swap(ints,indexOfLargest,lastUnsortedIndex);
+    }
   }
 }
