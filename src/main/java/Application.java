ab7df954969b8b5e14b76e19e2b3bdaaca094965
Micah Newsum <mnewsum@glenair.com>
2021-03-02 08:18:10 -0800

    Add method to created unsorted array.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,11 @@
 public class Application {
   public static void main(String[] args) {
+    System.out.println("-----Bubble Sort-----");
+    int[] ints = createUnsortedArray();
     
+  }
+
+  public static int[] createUnsortedArray(){
+    return new int[] {20, 35, -15, 7, 55, 1, -22};
   }
 }
